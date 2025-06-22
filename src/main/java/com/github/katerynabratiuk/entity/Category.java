3477b8416c65f37af3e33d10e400ab2f0bb59879
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-22 13:22:50 +0300

    Add Category entity

---
+++ src/main/java/com/github/katerynabratiuk/entity/Category.java
@@ -0,0 +1,43 @@
+package com.github.katerynabratiuk.entity;
+
+public class Category {
+
+    Integer id;
+    String name;
+
+    public Category() {
+    }
+
+    public Category(Integer id) {
+        this.id = id;
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
